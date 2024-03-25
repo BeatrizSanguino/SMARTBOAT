6f9158e1f247d96dce0d54ca5b31daaebf5c6620
BeatrizSanguino <92654843+BeatrizSanguino@users.noreply.github.com>
2024-03-25 22:49:31 +0100

    Add files via upload

--- Robot/robot_control_joystick_GPS/robot_subscribe_control.py
+++ Robot/robot_control_joystick_GPS/robot_subscribe_control.py
@@ -1,190 +1,145 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-import random
-import RPi.GPIO as GPIO
-import serial
 import time
+import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import numpy as np
 
+	
+def on_log(client, userdata, level, buf):
+    print("log: "+buf)
 
 def on_connect(client, userdata, flags, rc):
     if rc==0:
         client.connected_flag=True
-        print("Connected OK")
+        print("connected OK")
     else:
-        print("Bad connection. Return code=", rc)
+        print("Bad connection Return code=", rc)
 
-def on_disconnect(client, userdata, flags, rc):
-    print("Disconnected result code "+str(rc))
+def on_disconnect(client, userdata, flags, rc=0):
+    print("Diconnected result code "+str(rc))
 
-# Serial port configuration
-ser = serial.Serial('/dev/ttyUSB2',115200) #open serial port
-ser.flushInput()
+def on_message(client, userdata, message):
+    global M1
+    global M2
+    msg_aux = message.payload.decode("utf-8")
+    msg = msg_aux.split(",")
+    M1=float(msg[0])
+    M2=float(msg[1])
+ 
 
-f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','w')
+time.sleep(30)
 
-ser.write(('ATE0' + '\r\n').encode())
+# Initialization of motor actions
+M1=0.0
+M2=0.0
     
-f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-f.write("ser_in_wainting"+str(ser.in_waiting)+"\n")
-f.close()
-#print(ser.in_waiting)
-while not ser.in_waiting:
-    pass
-
-f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-f.write("ser_in_wainting-after"+str(ser.in_waiting)+"\n")
-f.close()
-#print(ser.in_waiting)
-while ser.in_waiting:
-    #response = ser.read(ser.in_waiting).decode()
-    response = ser.read_all().decode()
-    f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-    f   .write("response:"+response+"\n")
-    f.close()
-    #print("response:",response)
-    
-        
-
-ser.write(('AT+CPIN="6866"' + '\r\n').encode())
-f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-f.write('PIN-waiting response\n')
-f.close()
-#print("waiting response\n")
-
-f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-f.write("ser_in_wainting"+str(ser.in_waiting)+"\n")
-f.close()
-#print(ser.in_waiting)
-while not ser.in_waiting:
-    pass
-
-f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-f.write("ser_in_wainting-after"+str(ser.in_waiting)+"\n")
-f.close()
-#print(ser.in_waiting)
-while ser.in_waiting:
-    response = ser.read(ser.in_waiting).decode()
-    #response = ser.read_all().decode()
-    f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-    f   .write("response:"+response+"\n")
-    f.close()
-    #print("response:",response)
-    
-time.sleep(5)
-
-mqtt.Client.connected_flag=False
-    
-broker = "broker.hivemq.com"
-client_id_name = "robot_gps"+str(np.random.random()*1000)
-client = mqtt.Client(client_id=client_id_name, clean_session=True, userdata=None)
-    
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-    
-client.connect(broker, port=1883, keepalive=60)
-    
-client.loop_start()
-    
-while not client.connected_flag:
-    f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-    f.write("MQTT: in wait loop\n")
-    f.close()
-    time.sleep(1)
-    
-f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-f.write("MQTT: connected\n")
-f.close()
-
+# Set the GPIO modes
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(6,GPIO.OUT)
-time.sleep(0.1)
-GPIO.output(6,GPIO.HIGH)
-time.sleep(2)
-GPIO.output(6,GPIO.LOW)
-time.sleep(20)
 
-command='AT+CGPS=1,1'
-ser.write((command + '\r\n').encode())
-f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-f.write("ser_in_wainting-AT+CGPS=1,1"+str(ser.in_waiting)+"\n")
-f.close()
-#print(ser.in_waiting)
-while not ser.in_waiting:
-    pass
+# Set variables for the GPIO motor pins
+pinMotorA1 = 19
+pinMotorA2 = 13
+pinMotorA_EN = 26
+pinMotorB1 = 6
+pinMotorB2 = 5
+pinMotorB_EN = 12
 
-f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-f.write("ser_in_wainting-AT+CGPS1,1-after"+str(ser.in_waiting)+"\n")
-f.close()
-#print(ser.in_waiting)
-while ser.in_waiting:
-    #response = ser.read(ser.in_waiting).decode()
-    response = ser.read_all().decode()
-    f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-    f   .write("response-AT+CGPS1,1:"+response+"\n")
-    f.close()
-    #print("response:",response)
+freq = 40 # how many times to turn the pin on and off each second
+speed_factor = 10.0 # percentage of the maximum speed
 
-freq = 0.5
-period = 1/freq
-t0 = time.perf_counter()
-time_counter=t0
-aux=True
-while aux:
-    now = time.perf_counter()
-    time_elapse = now - t0
+# Set the GPIO Pin mode to be Output
+GPIO.setup(pinMotorA1, GPIO.OUT)
+GPIO.setup(pinMotorA2, GPIO.OUT)
+GPIO.setup(pinMotorA_EN, GPIO.OUT)
+GPIO.setup(pinMotorB1, GPIO.OUT)
+GPIO.setup(pinMotorB2, GPIO.OUT)
+GPIO.setup(pinMotorB_EN, GPIO.OUT)
+
+
+pwm_B = GPIO.PWM(pinMotorB_EN, freq)
+pwm_A = GPIO.PWM(pinMotorA_EN, freq)
+
+
+# Start the software PWM with a duty cycle of 0 (i.e. not moving)
+pwm_B.start(0)
+pwm_A.start(0)
+
+
+# MQTT Connection
+mqtt.Client.connected_flag=False
+
+broker = "broker.hivemq.com" #external broker
+
+client_id_name = "robot_"+str(np.random.random()*1000)
+client = mqtt.Client(client_id=client_id_name, clean_session=True, userdata=None)
+print("created a new client")
+
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+#client.on_log = on_log
+client.on_message = on_message
+
+print("connecting to broker")
+client.connect(broker, port=1883, keepalive=60)
+
+client.loop_start() 
+
+while not client.connected_flag:
+    print("in wait loop")
+    time.sleep(1)
+print("in main loop")
+
+# Subscribing to motor action topic
+topic1="robot/motor/action"
+print("subscribing to topic",topic1)
+client.subscribe(topic1,qos=2)
+
+
+while True:
+    #print(M1,M2)
     
-    ser.flushInput()
-    ser.flushOutput()
-    command="AT+CGPSINFO"
-    ser.write((command + '\r\n').encode())
+    # Action for motor 1
+    if M1<0: # motor 1 backward
+        if round(M1,4)<-1:
+            M1=-1.0
+        GPIO.output(pinMotorA1, GPIO.HIGH)
+        GPIO.output(pinMotorA2, GPIO.LOW)
+        dutycycle = abs(round(M1,4))*speed_factor #how long the pin stays on each cycle, as a percentage
+        pwm_A.ChangeDutyCycle(dutycycle)
     
-    f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-    f.write("waiting GPS-AT+CGPSINFO\n")
-    f.close()
-    #print("waiting GPS")
-
-    while not ser.in_waiting:
-        pass
+    elif M1>0: # motor 1 forward
+        if round(M1,4)>1:
+            M1=1
+        GPIO.output(pinMotorA1, GPIO.LOW)
+        GPIO.output(pinMotorA2, GPIO.HIGH)
+        dutycycle = abs(round(M1,4))*speed_factor
+        pwm_A.ChangeDutyCycle(dutycycle)
+    
+    elif M1==0: # motor 1 stopped
+        GPIO.output(pinMotorA1, GPIO.LOW)
+        GPIO.output(pinMotorA2, GPIO.LOW)
         
-    f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-    f.write("AT+CGPSINFO-ser_in_waiting:"+str(ser.in_waiting)+"\n")
-    f.close()
-    #while ser.in_waiting:
-    response = ser.read(ser.in_waiting).decode()
-    f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-    f.write("AT+CGPSINFO-response:"+response+"\n")
-    f.close()
+	# Action for motor 2
+    if M2<0: # motor 2 backward
+        if round(M2,4)<-1:
+            M2=-1.0
+        GPIO.output(pinMotorB1, GPIO.HIGH)
+        GPIO.output(pinMotorB2, GPIO.LOW)
+        dutycycle = abs(round(M2,4))*speed_factor
+        pwm_B.ChangeDutyCycle(dutycycle)
+    
+    elif M2>0: # motor 2 forward
+        if round(M2,4)>1:
+            M2=1
+        GPIO.output(pinMotorB1, GPIO.LOW)
+        GPIO.output(pinMotorB2, GPIO.HIGH)
+        dutycycle = abs(round(M2,4))*speed_factor
+        pwm_B.ChangeDutyCycle(dutycycle)
+    
+    elif M2==0: # motor 2 stopped
+        GPIO.output(pinMotorB1, GPIO.LOW)
+        GPIO.output(pinMotorB2, GPIO.LOW)
 
-    if response.find("+CGPSINFO:")!=-1:
-        if ',,,,,,' in response:
-            f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-            f.write('get_fps_position-GPS is not ready\n')
-            f.close()
-        else:
-            index = response.find("+CGPSINFO:")
-            cleaned = response[11+index:]
-            f=open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-            f.write("get_gps_position-cleaned:"+cleaned+"\n")
-            f.close()
-            latitude = float(cleaned[:2]) + (float(cleaned[2:11])/60)
-            NorthOrSouth = cleaned[12]
-			
-            longitude = float(cleaned[14:17]) + (float(cleaned[17:26])/60)
-            EastOrWest = cleaned[27]
 
-            if NorthOrSouth == 'S': latitude = -latitude
-            if EastOrWest == 'W': longitude = -longitude
-			
-            #print(latitude,longitude)
-			
-            pos = str(latitude) + ", " + str(longitude)
-            if time_elapse > period:
-                client.publish(topic="robot/GPS_position", payload=pos, qos=2, retain=False)
-                t0 = time.perf_counter()
-            f = open('/home/pi/SmartBoat/robot_control_joystick_GPS/test.log','a')
-            f.write('get_gps_position-Latitude:'+str(latitude)+"Longitude:"+str(longitude)+"\n")
-            f.close()
-            
+client.loop_stop()
+client.disconnect()
